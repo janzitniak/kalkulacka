3506f7a086d81b445779ea2bce373b9fcbb1c935
Ján Žitniak on GitHub.com <54016618+janzitniak@users.noreply.github.com>
2025-04-12 09:18:32 +0200

    Update VypocetVzorca.java
    Úprava vzorca, delenie desatinného čísla.

--- src/main/java/VypocetVzorca.java
+++ src/main/java/VypocetVzorca.java
@@ -6,7 +6,7 @@ public class VypocetVzorca {
     public static void main(String[] args) {
         ScriptEngineManager factory = new ScriptEngineManager();
         ScriptEngine engine = factory.getEngineByName("nashorn");
-        String vzorec = "1+2*3";
+        String vzorec = "1+(2*3)/0.3";
         try {
             System.out.println(engine.eval(vzorec)); // vysledok Stringu, ktory obsahuje vzorec vypisem
         } catch (ScriptException e) {
